601d812de6d4ebd7a425f6f67a4804082b5d019d
Balazs Meszaros <meszib@gmail.com>
2025-11-30 13:10:22 +0100

    [Quasar] Add string operation context to provide application wide string list/builder pooling.

---
+++ source/engine/Quasar/Utilities/IStringOperationContext.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IStringOperationContext.cs" company="Space Development">
+//      Copyright (c) Space Development. All rights reserved.
+// </copyright>
+// <summary>
+//     This file is subject to the terms and conditions defined in
+//     file 'LICENSE.txt', which is part of this source code package.
+// </summary>
+// <author>Balazs Meszaros</author>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text;
+
+using Space.Core.Collections;
+
+namespace Quasar.Utilities
+{
+    /// <summary>
+    /// Represents the context for string operations.
+    /// </summary>
+    public interface IStringOperationContext
+    {
+        /// <summary>
+        /// Gets the string builder pool.
+        /// </summary>
+        IPool<StringBuilder> BuilderPool { get; }
+
+        /// <summary>
+        /// Gets the string list pool.
+        /// </summary>
+        IPool<List<string>> ListPool { get; }
+    }
+}
---
+++ source/engine/Quasar/Utilities/Internals/StringOperationContext.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StringOperationContext.cs" company="Space Development">
+//      Copyright (c) Space Development. All rights reserved.
+// </copyright>
+// <summary>
+//     This file is subject to the terms and conditions defined in
+//     file 'LICENSE.txt', which is part of this source code package.
+// </summary>
+// <author>Balazs Meszaros</author>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text;
+
+using Space.Core.Collections;
+using Space.Core.DependencyInjection;
+
+namespace Quasar.Utilities.Internals
+{
+    /// <summary>
+    /// String operation context implementation.
+    /// </summary>
+    /// <seealso cref="IStringOperationContext" />
+    [Export(typeof(IStringOperationContext))]
+    [Singleton]
+    internal sealed class StringOperationContext : IStringOperationContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringOperationContext"/> class.
+        /// </summary>
+        /// <param name="poolFactory">The pool factory.</param>
+        public StringOperationContext(IPoolFactory poolFactory)
+        {
+            BuilderPool = poolFactory.Create(true, () => new StringBuilder(), x => x.Clear());
+            ListPool = poolFactory.Create(true, () => new List<string>(), x => x.Clear());
+        }
+
+
+        /// <inheritdoc/>
+        public IPool<StringBuilder> BuilderPool { get; }
+
+        /// <inheritdoc/>
+        public IPool<List<string>> ListPool { get; }
+    }
+}
