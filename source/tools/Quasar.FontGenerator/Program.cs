f021456f770bc516f20a8c1f61e31c841463800e
Balazs Meszaros <meszib@gmail.com>
2025-12-09 22:28:46 +0100

    [Fonts] Add generator tool's project

---
+++ source/tools/Quasar.FontGenerator/Program.cs
@@ -0,0 +1,34 @@
+//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Space Development">
+//      Copyright (c) Space Development. All rights reserved.
+// </copyright>
+// <summary>
+//     This file is subject to the terms and conditions defined in
+//     file 'LICENSE.txt', which is part of this source code package.
+// </summary>
+// <author>Balazs Meszaros</author>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Windows.Forms;
+
+namespace Quasar.FontGenerator
+{
+    /// <summary>
+    /// The main entry object for the application.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            ////Application.Run(new MainForm());
+        }
+    }
+}
---
+++ source/tools/Quasar.FontGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Space Development">
+//      Copyright (c) Space Development. All rights reserved.
+// </copyright>
+// <summary>
+//     This file is subject to the terms and conditions defined in
+//     file 'LICENSE.txt', which is part of this source code package.
+// </summary>
+// <author>Balazs Meszaros</author>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyTitle("Quasar 3D Engine -  Font Generator Tool")]
+[assembly: AssemblyDescription("Quasar 3D engine's font pack generator tool")]
+[assembly: SupportedOSPlatform("windows")]
